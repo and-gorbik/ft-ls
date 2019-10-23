75e79b5cc22e23d6f064222754cff43a81fb7911
and-gorbik <and-gorbik@mail.ru>
2019-10-23 21:59:40 +0300

    fix initialization of flags

--- srcs/flags.c
+++ srcs/flags.c
@@ -2,11 +2,6 @@
 
 static int  set_flags(t_flags *flags, char *src)
 {
-	flags->l = 0;
-	flags->r = 0;
-	flags->rr = 0;
-	flags->t = 0;
-	flags->a = 0;
 	while (*(++src)) {
 		switch (*src) {
 		case 'l':
@@ -36,11 +31,17 @@ int         parse_flags(t_flags *flags, int argc, char **argv)
 {
 	int		i;
 
+	flags->l = 0;
+	flags->r = 0;
+	flags->rr = 0;
+	flags->t = 0;
+	flags->a = 0;
 	i = 0;
 	while (++i < argc && argv[i][0] == '-' && argv[i][1])
 	{
 		if (!set_flags(flags, argv[i]))
             return (0);
+		++i;
 	}
 
 	return (1);
